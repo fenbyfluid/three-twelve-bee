09471c2bea684b2afe0991ef6dd267dcf653ab8b
Erin <fenbyfluid@gmail.com>
2021-01-29 18:54:22 +0000

    Update branding bits

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { H1 } from "@blueprintjs/core";
+import logo from "./logo.png";
 
 function App() {
-  return (
-    <H1>Hi.</H1>
-  );
+  const styles: React.CSSProperties = {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  return <div style={styles}>
+    <img src={logo} alt="Three Twelve Bee Logo" width={512} height={256} />
+    <H1>Hi there.</H1>
+  </div>;
 }
 
 export default App;
